a447e1b82d5ce039e496fa18836019d79a3953a0
agus grandon <agustina107.grandon@gmail.com>
2026-06-28 22:04:33 -0300

    agrege validacion de datos

--- alumnos.py
+++ alumnos.py
@@ -18,9 +18,20 @@ def registrar_alumno():
                 existe_dni = True
 
     nombre = input("Ingresa el nombre del alumno: ")
+    while len(nombre) == 0:
+        nombre = input("El dato no puede estar vacio, ingrese el nombre del alumno: ")
+
     apellido = input("Ingresa el apellido del alumno: ")
+    while len(apellido) == 0:
+        apellido = input("El dato no puede estar vacio, ingrese el apellido del alumno: ")
+
     edad = int(input("Ingresa la edad del alumno: "))
+    while edad <= 0:
+     edad = int(input("Error al ingresar la edad, intentelo nuevamente: "))
+
     nota = int(input("Ingresa la nota del alumno: "))
+    while nota <= 0 or nota >= 10:
+        nota = int(input("Error, la nota debe estar entre 1 y 10: "))
 
     alumno["dni"] = dni
     alumno["nombre"] = nombre
