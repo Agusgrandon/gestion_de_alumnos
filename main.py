d2aa801b783f216b082072ce0d1a9bdcf32d152a
agus grandon <agustina107.grandon@gmail.com>
2026-06-28 21:19:42 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+continuar = True
+
+while continuar:
+    opcion = int(input("ingresa la opcion"))
+
+    match opcion: 
+        case 2:
+            pass
+        case _:
+            print("La opcion ingresada no es correcta, intentelo nuevamente")
