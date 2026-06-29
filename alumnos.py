46f1d2451f667b79e7205502b08cb09b62864a35
agus grandon <agustina107.grandon@gmail.com>
2026-06-28 21:57:21 -0300

    agrege validacion dni

--- alumnos.py
+++ alumnos.py
@@ -5,17 +5,33 @@ def registrar_alumno():
 
     alumno = {}
 
-    dni = int(input("Ingresa el DNI: "))
-    nombre = input("Agrega el nombre")
-    apellido = input("Ingresa el apellido: ")
+    dni = int(input("Ingresa el DNI del alumno: "))
+    existe_dni = False
+    for alumnos in lista:
+        if alumnos["dni"] == dni:
+            existe_dni = True
+    while existe_dni == True:
+        dni = int(input("Ese DNI ya existe, reingrese el DNI: "))
+        existe_dni = False
+        for alumnos in lista:
+            if alumnos["dni"] == dni:
+                existe_dni = True
+
+    nombre = input("Ingresa el nombre del alumno: ")
+    apellido = input("Ingresa el apellido del alumno: ")
+    edad = int(input("Ingresa la edad del alumno: "))
+    nota = int(input("Ingresa la nota del alumno: "))
 
     alumno["dni"] = dni
     alumno["nombre"] = nombre
     alumno["apellido"] = apellido
+    alumno["edad"] = edad
+    alumno["nota"] = nota
+
 
     lista.append(alumno)
 
     guardar_datos(lista)
-    print("alumno ok")
+   
 
 registrar_alumno()
